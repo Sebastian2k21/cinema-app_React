8e9d38649e4e727d3cfcb6d7452f3543a9ba22ae
Sebastian Żądło <seba0071@interia.pl>
2023-11-21 10:34:00 +0100

    add comments , update posters

--- src/components/roots/MovieDetails.tsx
+++ src/components/roots/MovieDetails.tsx
@@ -5,10 +5,16 @@ interface Poster {
     file_path: string;
 }
 
+interface Comment {
+    email: string
+    body: string
+}
+
 const MovieDetails = () => {
     const poster_path = "https://www.themoviedb.org/t/p/w220_and_h330_face/";
     const [movie, setMovie] = useState({title: "", overview: "", poster_path: ""});
     const [posters, setPosters] = useState<Poster[]>([]);
+    const [comments, setComments] = useState<Comment[]>([]);
     const id = useParams().id;
 
     const getMovie = async () => {
@@ -18,23 +24,48 @@ const MovieDetails = () => {
 
         const imgResponse = await fetch(`https://api.themoviedb.org/3/movie/${id}}/images?api_key=a84909a3156caf84337832e7fe15e20c`)
         const imgData = await imgResponse.json();
-        setPosters(imgData["posters"])
+        let posters = imgData["posters"]
+
+        if(posters.length > 5) {
+            posters = posters.slice(0, 5)
+            console.log("OK")
+        }
+        setPosters(posters)
+    }
+
+    const getComments = async () => {
+        const newId = parseInt(id as string) % 100
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${newId}/comments`)
+        if(response.ok) {
+            const data = await response.json();
+            setComments(data);
+        }
     }
 
     useEffect(() => { 
         getMovie();
+        getComments();
      }, []);
 
    return (
    <div className='DetailsStyle'>
        <h1>{movie.title}</h1>
-       <img  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+       <img className='DetailsStyleImg'  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
        <p>{movie.overview}</p>
 
-        <div style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr"}}>
-            {posters.map(pos => (<div>
-                    <img src={poster_path + pos.file_path} alt="poster" />
-                </div>))}
+        <h3>Comments</h3>
+        <div>
+            {comments.map(comment => <p>
+                <div>{comment.email}</div>
+                <div>{comment.body}</div>
+            </p>)}
+        </div>
+        
+        <h3>Posters</h3>
+        <div className="poster-grid">
+            {posters.map(pos => (
+                    <img className="poster" src={poster_path + pos.file_path} alt="poster" />
+                ))}
         </div>
 
     </div>)
